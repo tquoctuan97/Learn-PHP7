7d6e835d99d15e58fabaa62c52749d450ac1a3d3
global mind developer <globalminddeveloper@globals-iMac.local>
2019-06-24 17:46:48 +0700

    PHP Null

---
+++ Data Types/tryphp_datatypes_null.php
@@ -0,0 +1,15 @@
+<?php 
+    // Null is a special data type which can have only one value: NULL.
+
+    // A variable of data type NULL is a variable that has no value assigned to it.
+
+    // Tip: If a variable is created without a value, it is automatically assigned a value of NULL.
+
+    // Variables can also be emptied by setting the value to NULL:
+
+    $x = "Hello world!";
+    $x = null;
+    $y;
+    var_dump($x);
+    var_dump($y);
+?>
