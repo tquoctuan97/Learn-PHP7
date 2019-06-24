ec658621e32754a28b6182591afa31a77d5cf9b6
global mind developer <globalminddeveloper@globals-iMac.local>
2019-06-24 17:27:21 +0700

    PHP Integer and using var_dump()

---
+++ Data Types/tryphp_datatypes_integer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    // A string is a sequence of characters, like "Hello world!".
+    // A string can be any text inside quotes. You can use single or double quotes:
+    
+       $x = 5985;
+       var_dump($x);
+    ?>
+</body>
+</html>
