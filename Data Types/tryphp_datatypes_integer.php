9f9eacad0978d9fd938d684cb84bfb2151d926dc
global mind developer <globalminddeveloper@globals-iMac.local>
2019-06-24 17:30:37 +0700

    PHP Float

--- Data Types/tryphp_datatypes_integer.php
+++ Data Types/tryphp_datatypes_integer.php
@@ -2,8 +2,15 @@
 <html>
 <body>
     <?php
-    // A string is a sequence of characters, like "Hello world!".
-    // A string can be any text inside quotes. You can use single or double quotes:
+    // An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+
+    // Rules for integers:
+    
+    // An integer must have at least one digit
+    // An integer must not have a decimal point
+    // An integer can be either positive or negative
+    // Integers can be specified in three formats: decimal (10-based), hexadecimal (16-based - prefixed with 0x) or octal (8-based - prefixed with 0)
+    // In the following example $x is an integer. The PHP var_dump() function returns the data type and value:
     
        $x = 5985;
        var_dump($x);
