2f8ac9421a9fab305934998dc92ee9e2f79af002
global mind developer <globalminddeveloper@globals-iMac.local>
2019-06-24 17:22:20 +0700

    PHP String

---
+++ Data Types/tryphp_datatypes_string.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    // A string is a sequence of characters, like "Hello world!".
+    // A string can be any text inside quotes. You can use single or double quotes:
+    
+        $x = "Hello world!";
+        $y = 'Hello world!';
+        
+        echo $x;
+        echo "<br>";
+        echo $y;
+    ?>
+</body>
+</html>
